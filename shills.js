9606498ce3c2b24b6b9051f8cb233b576df456d0
majorchampx1 <majorchampx1@gmail.com>
2016-08-25 16:03:06 -0400

    Update shills.js

--- shills.js
+++ shills.js
@@ -1,14 +1,6 @@
 var shills = [
-    {
-    "username":"David-03",
-    },
-    {
-    "username":"evanluke-01",
-    },
-    {
-    "username":"john-033",
-    },
-    {
-    "username":"william033",
-    }
+    "David-03",
+    "evanluke-01",
+    "john-033",
+    "william033"
 ];
