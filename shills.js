cc02562dca32719bb55a274a83d44a3dc0e32b5e
majorchampx1 <majorchampx1@gmail.com>
2016-08-25 15:51:05 -0400

    create shills.js

---
+++ shills.js
@@ -0,0 +1,14 @@
+var shills = [
+    {
+    "username":"David-03",
+    },
+    {
+    "username":"evanluke-01",
+    },
+    {
+    "username":"john-033",
+    },
+    {
+    "username":"william033",
+    }
+];
